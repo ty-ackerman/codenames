b377d1e2453c34c5b6c658d2f89fb37c8c14b102
Ty Ackerman <ty.ackerman@Tys-MacBook-Pro.local>
2019-11-01 07:47:31 -0400

    [NodeJS] - Backend has been created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
 
 import Board from './components/Board';
 
 function App() {
+	useEffect(() => {
+		async function fetchData() {
+			try {
+				const response = await axios.get('/healthcheck');
+				console.log(response.data);
+			} catch (error) {
+				console.log(error);
+			}
+		}
+
+		fetchData();
+	});
 	return (
 		<div className="App">
 			<p>Test</p>
